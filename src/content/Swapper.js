5fa43b58de713f1c8423c24118381e4be53d9169
Sebastian Velasquez <anse23@hotmail.com>
2024-05-01 21:19:54 -0500

    refactors: Move swap function to context

--- src/content/Swapper.js
+++ src/content/Swapper.js
@@ -1,26 +1,9 @@
-import React, { useCallback, useState } from 'react';
+import React from 'react';
 import { Box } from 'rebass';
 import { useHiderContext } from './useHiderContext';
 
 export default function Swapper() {
-  const { topRef, bottomRef, controlsHeight } = useHiderContext();
-
-  const [swapped, setSwapped] = useState(false);
-  const swap = useCallback(() => {
-    if (bottomRef.current && topRef.current) {
-      const bottomPointerEvents = bottomRef.current.style.pointerEvents;
-      const topPointerEvents = topRef.current.style.pointerEvents;
-      const bottomOpacity = bottomRef.current.style.opacity;
-      const topOpacity = topRef.current.style.opacity;
-
-      bottomRef.current.style.pointerEvents = topPointerEvents;
-      topRef.current.style.pointerEvents = bottomPointerEvents;
-      bottomRef.current.style.opacity = topOpacity;
-      topRef.current.style.opacity = bottomOpacity;
-
-      setSwapped(!swapped);
-    }
-  }, [bottomRef, topRef, setSwapped, swapped]);
+  const { controlsHeight, swap, swapped } = useHiderContext();
 
   return (
     <Box
--- src/content/useHiderContext.js
+++ src/content/useHiderContext.js
@@ -1,4 +1,4 @@
-import React, { useContext, createContext } from 'react';
+import React, { useContext, createContext, useCallback, useState } from 'react';
 
 const HiderContext = createContext({});
 
@@ -10,10 +10,29 @@ export default function HiderProvider({
   controlsHeight,
   children,
 }) {
+  const [swapped, setSwapped] = useState(false);
+  const swap = useCallback(() => {
+    if (bottomRef.current && topRef.current) {
+      const bottomPointerEvents = bottomRef.current.style.pointerEvents;
+      const topPointerEvents = topRef.current.style.pointerEvents;
+      const bottomOpacity = bottomRef.current.style.opacity;
+      const topOpacity = topRef.current.style.opacity;
+
+      bottomRef.current.style.pointerEvents = topPointerEvents;
+      topRef.current.style.pointerEvents = bottomPointerEvents;
+      bottomRef.current.style.opacity = topOpacity;
+      topRef.current.style.opacity = bottomOpacity;
+
+      setSwapped(!swapped);
+    }
+  }, [bottomRef, topRef, setSwapped, swapped]);
+
   const context = {
     controlsHeight,
     topRef,
     bottomRef,
+    swap,
+    swapped,
   };
   return (
     <HiderContext.Provider value={context}>{children}</HiderContext.Provider>
